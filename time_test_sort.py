be91ca9b3e402e706af3322a7d40023f35d4f124
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-10-29 20:07:51 -0300

    add time_test_sort.py

---
+++ time_test_sort.py
@@ -0,0 +1,124 @@
+# imports
+from bubble_sort import bubble_sort
+import time
+from listas_desordenadas import (
+    lista_1000_Desordenado,
+    lista_100_Desordenado,
+    lista_50_Desordenado,
+    lista_25_Desordenado,
+    lista_10_Desordenado
+)
+from listas_quase_ordenadas import (
+    lista_1000_QuaseOrdenado,
+    lista_100_QuaseOrdenado,
+    lista_50_QuaseOrdenado,
+    lista_25_QuaseOrdenado,
+    lista_10_QuaseOrdenado
+)
+from listas_ordenadas_inversamente import (
+    lista_1000_InversamenteOrdenado,
+    lista_100_InversamenteOrdenado,
+    lista_50_InversamenteOrdenado,
+    lista_25_InversamenteOrdenado,
+    lista_10_InversamenteOrdenado
+)
+
+
+
+
+# Sort ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Sort: Lista Desordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Sort: Lista Desordenada: lista_10_Desordenado
+start_time_10_desord_sort = time.time()
+lista_10_Desordenado.sort()
+print(lista_10_Desordenado)
+end_time_10_desord_sort = time.time()
+
+# Sort: Lista Desordenada: lista_25_Desordenado
+start_time_25_desord_sort = time.time()  
+lista_25_Desordenado.sort()
+print(lista_25_Desordenado)
+end_time_25_desord_sort = time.time()
+
+# Sort: Lista Desordenada: lista_50_Desordenado
+start_time_50_desord_sort = time.time()
+lista_50_Desordenado.sort()
+print(lista_50_Desordenado)
+end_time_50_desord_sort = time.time()
+
+# Sort: Lista Desordenada: lista_100_Desordenado
+start_time_100_desord_sort = time.time()
+lista_100_Desordenado.sort()
+print(lista_100_Desordenado)
+end_time_100_desord_sort = time.time()
+
+# Sort: Lista Desordenada: lista_1000_Desordenado
+start_time_1000_desord_sort = time.time()
+lista_1000_Desordenado.sort()
+print(lista_1000_Desordenado)
+end_time_1000_desord_sort = time.time()
+
+# Sort: Lista quase ordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Sort: Lista quase ordenada: lista_10_QuaseOrdenado
+start_time_10_QuaseOrd_sort = time.time()
+lista_10_QuaseOrdenado.sort()
+print(lista_10_QuaseOrdenado)
+end_time_10_QuaseOrd_sort = time.time()
+
+# Sort: Lista quase ordenada: lista_25_QuaseOrdenado
+start_time_25_QuaseOrd_sort = time.time()
+lista_25_QuaseOrdenado.sort()
+print(lista_25_QuaseOrdenado)
+end_time_25_QuaseOrd_sort = time.time()
+
+# Sort: Lista quase ordenada: lista_50_QuaseOrdenado
+start_time_50_QuaseOrd_sort = time.time()
+lista_50_QuaseOrdenado.sort()
+print(lista_50_QuaseOrdenado)
+end_time_50_QuaseOrd_sort = time.time()
+
+# Sort: Lista quase ordenada: lista_100_QuaseOrdenado
+start_time_100_QuaseOrd_sort = time.time()
+lista_100_QuaseOrdenado.sort()
+print(lista_100_QuaseOrdenado)
+end_time_100_QuaseOrd_sort = time.time()
+
+# Sort: Lista quase ordenada: lista_1000_QuaseOrdenado
+start_time_1000_QuaseOrd_sort = time.time()
+lista_1000_QuaseOrdenado.sort()
+print(lista_1000_QuaseOrdenado)
+end_time_1000_QuaseOrd_sort = time.time()
+
+# Sort: lista inversamente ordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Sort: lista inversamente ordenada: lista_10_InversamenteOrdenado
+start_time_10_InversamenteOrd_sort = time.time()
+lista_10_InversamenteOrdenado.sort()
+print(lista_10_InversamenteOrdenado)
+end_time_10_InversamenteOrd_sort = time.time()
+
+# Sort: lista inversamente ordenada: lista_25_InversamenteOrdenado
+start_time_25_InversamenteOrd_sort = time.time()
+lista_25_InversamenteOrdenado.sort()
+print(lista_25_InversamenteOrdenado)
+end_time_25_InversamenteOrd_sort = time.time()
+
+# Sort: lista inversamente ordenada: lista_50_InversamenteOrdenado
+start_time_50_InversamenteOrd_sort = time.time()
+lista_50_InversamenteOrdenado.sort()
+print(lista_50_InversamenteOrdenado)
+end_time_50_InversamenteOrd_sort = time.time()
+
+# Sort: lista inversamente ordenada: lista_100_InversamenteOrdenado
+start_time_100_InversamenteOrd_sort = time.time()
+lista_100_InversamenteOrdenado.sort()
+print(lista_100_InversamenteOrdenado)
+end_time_100_InversamenteOrd_sort = time.time()
+
+# Sort: lista inversamente ordenada: lista_1000_InversamenteOrdenado
+start_time_1000_InversamenteOrd_sort = time.time()
+lista_1000_InversamenteOrdenado.sort()
+print(lista_1000_InversamenteOrdenado)
+end_time_1000_InversamenteOrd_sort = time.time()

8a87b4ff6132243116dcc145b52319e98b6fa871
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-10-30 19:01:57 -0300

    update

--- graph_selection_sort_time.py
+++ graph_selection_sort_time.py
@@ -1,6 +1,7 @@
-#Import
 import matplotlib.pyplot as plt
+import numpy as np
 
+# Importando os tempos medidos em milissegundos
 from test_time_selection_sort import (
     result_time_lista_10_Desordenado_selection,
     result_time_lista_25_Desordenado_selection,
@@ -19,34 +20,47 @@ from test_time_selection_sort import (
     result_time_lista_1000_InversamenteOrd_selection,
 )
 
-# Tempos medidos em milissegundos
-tempos = [
-    result_time_lista_10_Desordenado_selection,
-    result_time_lista_25_Desordenado_selection,
-    result_time_lista_50_Desordenado_selection,
-    result_time_lista_100_Desordenado_selection,
-    result_time_lista_1000_Desordenado_selection,
-    result_time_lista_10_QuaseOrd_selection,
-    result_time_lista_25_QuaseOrd_selection,
-    result_time_lista_50_QuaseOrd_selection,
-    result_time_lista_100_QuaseOrd_selection,
-    result_time_lista_1000_QuaseOrd_selection,
-    result_time_lista_10_InversamenteOrd_selection,
-    result_time_lista_25_InversamenteOrd_selection,
-    result_time_lista_50_InversamenteOrd_selection,
-    result_time_lista_100_InversamenteOrd_selection,
-    result_time_lista_1000_InversamenteOrd_selection,
-]
-# Rótulos para os diferentes tamanhos de lista
-rotulos = [
-    "10 Desord", "25 Desord", "50 Desord", "100 Desord", "1000 Desord",
-    "10 Quase Ord", "25 Quase Ord", "50 Quase Ord", "100 Quase Ord", "1000 Quase Ord",
-    "10 Inver Ord", "25 Inver Ord", "50 Inver Ord", "100 Inver Ord", "1000 Invers Ord"
-]
-# Criando o gráfico
+tempos = {
+    'Desordenada': {
+        '10': result_time_lista_10_Desordenado_selection,
+        '25': result_time_lista_25_Desordenado_selection,
+        '50': result_time_lista_50_Desordenado_selection,
+        '100': result_time_lista_100_Desordenado_selection,
+        '1000': result_time_lista_1000_Desordenado_selection,
+    },
+    'Quase Ordenada': {
+        '10': result_time_lista_10_QuaseOrd_selection,
+        '25': result_time_lista_25_QuaseOrd_selection,
+        '50': result_time_lista_50_QuaseOrd_selection,
+        '100': result_time_lista_100_QuaseOrd_selection,
+        '1000': result_time_lista_1000_QuaseOrd_selection,
+    },
+    'Inversamente Ordenada': {
+        '10': result_time_lista_10_InversamenteOrd_selection,
+        '25': result_time_lista_25_InversamenteOrd_selection,
+        '50': result_time_lista_50_InversamenteOrd_selection,
+        '100': result_time_lista_100_InversamenteOrd_selection,
+        '1000': result_time_lista_1000_InversamenteOrd_selection,
+    },
+}
+
 plt.figure(figsize=(10, 6))
-plt.barh(rotulos, tempos, color='skyblue')
-plt.xlabel('Tempo (ms)')
-plt.title('Tempo de Execução do Selection Sort')
+
+# Definindo a largura das barras
+largura = 0.2
+posicoes = np.arange(len(tempos['Desordenada']))  # Posições no eixo Y
+
+# Plotando as barras horizontais para cada tipo
+for i, (tipo, tempos_lista) in enumerate(tempos.items()):
+    plt.barh(posicoes + i * largura, list(tempos_lista.values()), largura, label=tipo)
+
+# Adicionando detalhes ao gráfico
+plt.title('Comparação de Algoritmos de Ordenação - Selection Sort')
+plt.ylabel('Tamanho da Lista')
+plt.xlabel('Tempo de Execução (ms)')
+plt.yticks(posicoes + largura, list(tempos['Desordenada'].keys()))  # Ticks com os tamanhos
+plt.legend()
 plt.grid(axis='x')
+plt.tight_layout()
+
 plt.show()

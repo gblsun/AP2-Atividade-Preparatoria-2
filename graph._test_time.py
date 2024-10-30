03afb08ab249ba9dc35c4f18aab560e7d2802370
Guilherme Pinheiro dos Santos <guilherme.psantos.dev@gmail.com>
2024-10-30 17:51:05 -0300

    update

---
+++ graph._test_time.py
@@ -0,0 +1,98 @@
+#Import
+import random
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+from function_merge_sort import merge_sort as ms
+from function_bubble_sort import bubble_sort as bs
+from function_heap_sort import heap_sort as hs
+from function_insertion_sort import insertion_sort_modificado
+from function_quick_sort import quick_sort as qs
+from function_selection_sort import selection_sort as ss
+
+import time
+
+
+from listas_quase_ordenadas import (
+    lista_10_QuaseOrdenado,
+    lista_25_QuaseOrdenado,
+    lista_50_QuaseOrdenado,
+    lista_100_QuaseOrdenado,
+    lista_1000_QuaseOrdenado
+)
+
+from listas_desordenadas import (
+    lista_10_Desordenado,
+    lista_25_Desordenado,
+    lista_50_Desordenado,
+    lista_100_Desordenado,
+    lista_1000_Desordenado
+)
+
+from listas_ordenadas_inversamente import (
+    lista_10_InversamenteOrdenado,
+    lista_25_InversamenteOrdenado,
+    lista_50_InversamenteOrdenado,
+    lista_100_InversamenteOrdenado,
+    lista_1000_InversamenteOrdenado
+)
+
+from test_time_bubble_sort import end_time_10_desord_bubble_sort
+
+
+# Bubble_Sort = {}
+# Selection_Sort = {}
+# Insertion_Sort = {}
+# Heap_Sort = {}
+# Merge_Sort = {}
+# Quick_Sort = {}
+# Sort = {}
+
+# plt.figure(figsize=(10, 10))
+# plt.plot()
+mport matplotlib.pyplot as plt
+
+# Tempos de execução (substitua pelas suas variáveis)
+tempos = {
+    'Desordenada': {
+        '10': result_time_lista_10_Desordenado_bubble,
+        '25': result_time_lista_25_Desordenado_bubble,
+        '50': result_time_lista_50_Desordenado_bubble,
+        '100': result_time_lista_100_Desordenado_bubble,
+        '1000': result_time_lista_1000_Desordenado_bubble,
+    },
+    'Quase Ordenada': {
+        '10': result_time_lista_10_QuaseOrd_bubble,
+        '25': result_time_lista_25_QuaseOrd_bubble,
+        '50': result_time_lista_50_QuaseOrd_bubble,
+        '100': result_time_lista_100_QuaseOrd_bubble,
+        '1000': result_time_lista_1000_QuaseOrd_bubble,
+    },
+    'Inversamente Ordenada': {
+        '10': result_time_lista_10_InversamenteOrd_bubble,
+        '25': result_time_lista_25_InversamenteOrd_bubble,
+        '50': result_time_lista_50_InversamenteOrd_bubble,
+        '100': result_time_lista_100_InversamenteOrd_bubble,
+        '1000': result_time_lista_1000_InversamenteOrd_bubble,
+    },
+}
+
+# Criando o gráfico
+plt.figure(figsize=(10, 6))
+
+# Plotando cada tipo de lista
+for tipo, tempos_lista in tempos.items():
+    plt.plot(tempos_lista.keys(), tempos_lista.values(), marker='o', label=tipo)
+
+# Configurações do gráfico
+plt.title('Comparação de Algoritmos de Ordenação - Bubble Sort')
+plt.xlabel('Tamanho da Lista')
+plt.ylabel('Tempo de Execução (s)')
+plt.legend()
+plt.grid(True)
+plt.xticks(list(tempos['Desordenada'].keys()))  # Adiciona as chaves como ticks
+plt.tight_layout()
+
+# Exibindo o gráfico
+plt.show()

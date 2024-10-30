7c3e8a139cb778266853a1fb73f9fd3c6e56d159
Guilherme Pinheiro dos Santos <guilherme.psantos.dev@gmail.com>
2024-10-30 15:14:27 -0300

    update insertion sort result time complete

--- time_test_insertion_sort.py
+++ time_test_insertion_sort.py
@@ -32,94 +32,109 @@ from listas_ordenadas_inversamente import (
 
 # Insertion Sort: Lista Desordenada: lista_10_Desordenado
 start_time_10_desord_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_10_Desordenado))
+insertion_sort_modificado(lista_10_Desordenado)
 end_time_10_desord_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_10_Desordenado:", end_time_10_desord_insertion_sort_modificado - start_time_10_desord_insertion_sort_modificado)
+result_time_lista_10_desord_insertion_sort = end_time_10_desord_insertion_sort_modificado - start_time_10_desord_insertion_sort_modificado
+print("Tempo para ordenar lista_10_Desordenado:", result_time_lista_10_desord_insertion_sort)
 
 # Insertion Sort: Lista Desordenada: lista_25_Desordenado
 start_time_25_desord_insertion_sort_modificado = time.perf_counter()  
-print(insertion_sort_modificado(lista_25_Desordenado))
+insertion_sort_modificado(lista_25_Desordenado)
 end_time_25_desord_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_25_Desordenado:", end_time_25_desord_insertion_sort_modificado - start_time_25_desord_insertion_sort_modificado)
+result_time_lista_25_desord_insertion_sort = end_time_25_desord_insertion_sort_modificado - start_time_25_desord_insertion_sort_modificado
+print("Tempo para ordenar lista_25_Desordenado:", result_time_lista_25_desord_insertion_sort)
 
 # Insertion Sort: Lista Desordenada: lista_50_Desordenado
 start_time_50_desord_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_50_Desordenado))
+insertion_sort_modificado(lista_50_Desordenado)
 end_time_50_desord_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_50_Desordenado:", end_time_50_desord_insertion_sort_modificado - start_time_50_desord_insertion_sort_modificado)
+result_time_lista_50_desord_insertion_sort = end_time_50_desord_insertion_sort_modificado - start_time_50_desord_insertion_sort_modificado
+print("Tempo para ordenar lista_50_Desordenado:", result_time_lista_50_desord_insertion_sort)
 
 # Insertion Sort: Lista Desordenada: lista_100_Desordenado
 start_time_100_desord_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_100_Desordenado))
+insertion_sort_modificado(lista_100_Desordenado)
 end_time_100_desord_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_100_Desordenado:", end_time_100_desord_insertion_sort_modificado - start_time_100_desord_insertion_sort_modificado)
+result_time_lista_100_desord_insertion_sort = end_time_100_desord_insertion_sort_modificado - start_time_100_desord_insertion_sort_modificado
+print("Tempo para ordenar lista_100_Desordenado:", result_time_lista_100_desord_insertion_sort)
 
 # Insertion Sort: Lista Desordenada: lista_1000_Desordenado
 start_time_1000_desord_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_1000_Desordenado))
+insertion_sort_modificado(lista_1000_Desordenado)
 end_time_1000_desord_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_100_Desordenado:", end_time_100_desord_insertion_sort_modificado - start_time_100_desord_insertion_sort_modificado)
+result_time_lista_1000_desord_insertion_sort = end_time_1000_desord_insertion_sort_modificado - start_time_1000_desord_insertion_sort_modificado
+print("Tempo para ordenar lista_100_Desordenado:", result_time_lista_1000_desord_insertion_sort)
 
 # Insertion Sort: Lista quase ordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Insertion Sort: Lista quase ordenada: lista_10_QuaseOrdenado
 start_time_10_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_10_QuaseOrdenado))
+insertion_sort_modificado(lista_10_QuaseOrdenado)
 end_time_10_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_10_Quase_Ordenado:", end_time_10_QuaseOrd_insertion_sort_modificado - start_time_10_QuaseOrd_insertion_sort_modificado)
+result_time_lista_10_QuaseOrd_insertion_sort = end_time_10_QuaseOrd_insertion_sort_modificado - start_time_10_QuaseOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_10_Quase_Ordenado:", result_time_lista_10_QuaseOrd_insertion_sort )
 
 # Insertion Sort: Lista quase ordenada: lista_25_QuaseOrdenado
 start_time_25_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_25_QuaseOrdenado))
+insertion_sort_modificado(lista_25_QuaseOrdenado)
 end_time_25_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_25_Quase_Ordenado:", end_time_25_QuaseOrd_insertion_sort_modificado - start_time_25_QuaseOrd_insertion_sort_modificado)
+result_time_lista_25_QuaseOrd_insertion_sort = end_time_25_QuaseOrd_insertion_sort_modificado - start_time_25_QuaseOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_25_Quase_Ordenado:", result_time_lista_25_QuaseOrd_insertion_sort)
 
 # Insertion Sort: Lista quase ordenada: lista_50_QuaseOrdenado
 start_time_50_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_50_QuaseOrdenado))
+insertion_sort_modificado(lista_50_QuaseOrdenado)
 end_time_50_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_50_Quase_Ordenado:", end_time_50_QuaseOrd_insertion_sort_modificado - start_time_50_QuaseOrd_insertion_sort_modificado)
+result_time_lista_50_QuaseOrd_insertion_sort = end_time_50_QuaseOrd_insertion_sort_modificado - start_time_50_QuaseOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_50_Quase_Ordenado:", result_time_lista_50_QuaseOrd_insertion_sort)
 
 # Insertion Sort: Lista quase ordenada: lista_100_QuaseOrdenado
-start_time_100_QuaseOrd_insertion_sort = time.perf_counter()
-print(insertion_sort_modificado(lista_100_QuaseOrdenado))
+start_time_100_QuaseOrd_insertion_sort_modificado = time.perf_counter()
+insertion_sort_modificado(lista_100_QuaseOrdenado)
 end_time_100_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_100_Quase_Ordenado:", end_time_100_QuaseOrd_insertion_sort_modificado - start_time_10_QuaseOrd_insertion_sort_modificado)
+result_time_lista_100_QuaseOrd_insertion_sort = end_time_100_QuaseOrd_insertion_sort_modificado - start_time_100_QuaseOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_100_Quase_Ordenado:", result_time_lista_100_QuaseOrd_insertion_sort)
 
 # Insertion Sort: Lista quase ordenada: lista_1000_QuaseOrdenado
 start_time_1000_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_1000_QuaseOrdenado))
+insertion_sort_modificado(lista_1000_QuaseOrdenado)
 end_time_1000_QuaseOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_1000_Quase_Ordenado:", end_time_1000_QuaseOrd_insertion_sort_modificado - start_time_1000_QuaseOrd_insertion_sort_modificado)
+result_time_lista_1000_QuaseOrd_insertion_sort = end_time_1000_QuaseOrd_insertion_sort_modificado - start_time_1000_QuaseOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_1000_Quase_Ordenado:", result_time_lista_1000_QuaseOrd_insertion_sort)
 
 # Insertion Sort: lista inversamente ordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Insertion Sort: lista inversamente ordenada: lista_10_InversamenteOrdenado
 start_time_10_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_10_InversamenteOrdenado))
+insertion_sort_modificado(lista_10_InversamenteOrdenado)
 end_time_10_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_10_Inversamente_Ordenado:", end_time_10_InversamenteOrd_insertion_sort_modificado - start_time_10_InversamenteOrd_insertion_sort_modificado)
+result_time_lista_10_InversamenteOrd_insertion = end_time_10_InversamenteOrd_insertion_sort_modificado - start_time_10_InversamenteOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_10_Inversamente_Ordenado:", result_time_lista_10_InversamenteOrd_insertion )
 
 # Insertion Sort: lista inversamente ordenada: lista_25_InversamenteOrdenado
 start_time_25_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_25_InversamenteOrdenado))
+insertion_sort_modificado(lista_25_InversamenteOrdenado)
 end_time_25_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_25_Inversamente_Ordenado:", end_time_25_InversamenteOrd_insertion_sort_modificado - start_time_25_InversamenteOrd_insertion_sort_modificado)
+result_time_lista_25_InversamenteOrd_insertion = end_time_25_InversamenteOrd_insertion_sort_modificado - start_time_25_InversamenteOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_25_Inversamente_Ordenado:", result_time_lista_25_InversamenteOrd_insertion)
 
 # Insertion Sort: lista inversamente ordenada: lista_50_InversamenteOrdenado
 start_time_50_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_50_InversamenteOrdenado))
+insertion_sort_modificado(lista_50_InversamenteOrdenado)
 end_time_50_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_50_Inversamente_Ordenado:", end_time_50_InversamenteOrd_insertion_sort_modificado - start_time_50_InversamenteOrd_insertion_sort_modificado)
+result_time_lista_50_InversamenteOrd_insertion = end_time_50_InversamenteOrd_insertion_sort_modificado - start_time_50_InversamenteOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_50_Inversamente_Ordenado:", result_time_lista_50_InversamenteOrd_insertion )
 
 # Insertion Sort: lista inversamente ordenada: lista_100_InversamenteOrdenado
 start_time_100_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_100_InversamenteOrdenado))
+insertion_sort_modificado(lista_100_InversamenteOrdenado)
 end_time_100_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_100_Inversamente_Ordenado:", end_time_100_InversamenteOrd_insertion_sort_modificado - start_time_100_InversamenteOrd_insertion_sort_modificado)
+result_time_lista_100_InversamenteOrd_insertion = end_time_100_InversamenteOrd_insertion_sort_modificado - start_time_100_InversamenteOrd_insertion_sort_modificado
+print("Tempo para ordenar lista_100_Inversamente_Ordenado:", result_time_lista_100_InversamenteOrd_insertion)
 
 # Insertion Sort: lista inversamente ordenada: lista_1000_InversamenteOrdenado
 start_time_1000_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print(insertion_sort_modificado(lista_1000_InversamenteOrdenado))
+insertion_sort_modificado(lista_1000_InversamenteOrdenado)
 end_time_1000_InversamenteOrd_insertion_sort_modificado = time.perf_counter()
-print("Tempo para ordenar lista_1000_Inversamente_Ordenado:", end_time_1000_InversamenteOrd_insertion_sort_modificado - start_time_1000_InversamenteOrd_insertion_sort_modificado)
+result_time_lista_1000_InversamenteOrd_insertion = end_time_1000_InversamenteOrd_insertion_sort_modificado - start_time_1000_InversamenteOrd_insertion_sort_modificado 
+print("Tempo para ordenar lista_1000_Inversamente_Ordenado:", result_time_lista_1000_InversamenteOrd_insertion)

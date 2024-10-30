40e583e5b215ce6bdf407b55c56c538b115362be
Guilherme Pinheiro dos Santos <guilherme.psantos.dev@gmail.com>
2024-10-30 14:23:32 -0300

    complete result time heap

--- time_test_heap_sort.py
+++ time_test_heap_sort.py
@@ -34,32 +34,36 @@ from listas_ordenadas_inversamente import (
 start_time_10_desord_hs = time.perf_counter()
 print(hs(lista_10_Desordenado))
 end_time_10_desord_hs = time.perf_counter()
-
-print("Tempo para ordenar lista_10_Desordenado:", end_time_10_desord_hs - start_time_10_desord_hs)
+result_time_lista_10_Desordenado_hs = end_time_10_desord_hs - start_time_10_desord_hs
+print("Tempo para ordenar lista_10_Desordenado:", result_time_lista_10_Desordenado_hs )
 
 # Heap Sort: Lista Desordenada: lista_25_Desordenado
 start_time_25_desord_hs = time.perf_counter()  
 print(hs(lista_25_Desordenado))
 end_time_25_desord_hs = time.perf_counter()
-print("Tempo para ordenar lista_25_Desordenado:", end_time_25_desord_hs - start_time_25_desord_hs)
+result_time_lista_25_Desordenado_hs = end_time_25_desord_hs - start_time_25_desord_hs
+print("Tempo para ordenar lista_25_Desordenado:", result_time_lista_25_Desordenado_hs )
 
 # Heap Sort: Lista Desordenada: lista_50_Desordenado
 start_time_50_desord_hs = time.perf_counter()
 print(hs(lista_50_Desordenado))
 end_time_50_desord_hs = time.perf_counter()
-print("Tempo para ordenar lista_50_Desordenado:", end_time_50_desord_hs - start_time_50_desord_hs)
+result_time_lista_50_Desordenado_hs = end_time_50_desord_hs - start_time_50_desord_hs
+print("Tempo para ordenar lista_50_Desordenado:", result_time_lista_50_Desordenado_hs )
 
 # Heap Sort: Lista Desordenada: lista_100_Desordenado
 start_time_100_desord_hs = time.perf_counter()
 print(hs(lista_100_Desordenado))
 end_time_100_desord_hs = time.perf_counter()
-print("Tempo para ordenar lista_100_Desordenado:", end_time_100_desord_hs - start_time_100_desord_hs)
+result_time_lista_100_Desordenado_hs = end_time_100_desord_hs - start_time_100_desord_hs
+print("Tempo para ordenar lista_100_Desordenado:", result_time_lista_100_Desordenado_hs)
 
 # Heap Sort: Lista Desordenada: lista_1000_Desordenado
 start_time_1000_desord_hs = time.perf_counter()
 print(hs(lista_1000_Desordenado))
 end_time_1000_desord_hs = time.perf_counter()
-print("Tempo para ordenar lista_1000_Desordenado:", end_time_1000_desord_hs - start_time_1000_desord_hs)
+result_time_lista_1000_Desordenado_hs = end_time_1000_desord_hs - start_time_1000_desord_hs
+print("Tempo para ordenar lista_1000_Desordenado:", result_time_lista_1000_Desordenado_hs)
 
 # Heap Sort: Lista quase ordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -67,31 +71,36 @@ print("Tempo para ordenar lista_1000_Desordenado:", end_time_1000_desord_hs - st
 start_time_10_QuaseOrd_hs = time.perf_counter()
 print(hs(lista_10_QuaseOrdenado))
 end_time_10_QuaseOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_10_Quase_Ordenado:", end_time_10_QuaseOrd_hs - start_time_10_QuaseOrd_hs)
+result_time_lista_10_QuaseOrd_hs = end_time_10_QuaseOrd_hs - start_time_10_QuaseOrd_hs
+print("Tempo para ordenar lista_10_Quase_Ordenado:", result_time_lista_10_QuaseOrd_hs)
 
 # Heap Sort: Lista quase ordenada: lista_25_QuaseOrdenado
 start_time_25_QuaseOrd_hs = time.perf_counter()
 print(hs(lista_25_QuaseOrdenado))
 end_time_25_QuaseOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_25_Quase_Ordenado:", end_time_25_QuaseOrd_hs - start_time_25_QuaseOrd_hs)
+result_time_lista_25_QuaseOrd_hs = end_time_25_QuaseOrd_hs - start_time_25_QuaseOrd_hs
+print("Tempo para ordenar lista_25_Quase_Ordenado:", result_time_lista_25_QuaseOrd_hs)
 
 # Heap Sort: Lista quase ordenada: lista_50_QuaseOrdenado
 start_time_50_QuaseOrd_hs = time.perf_counter()
 print(hs(lista_50_QuaseOrdenado))
 end_time_50_QuaseOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_50_Quase_Ordenado:", end_time_50_QuaseOrd_hs - start_time_50_QuaseOrd_hs)
+result_time_lista_50_QuaseOrd_hs = end_time_50_QuaseOrd_hs - start_time_50_QuaseOrd_hs
+print("Tempo para ordenar lista_50_Quase_Ordenado:", result_time_lista_50_QuaseOrd_hs)
 
 # Heap Sort: Lista quase ordenada: lista_100_QuaseOrdenado
 start_time_100_QuaseOrd_hs = time.perf_counter()
 print(hs(lista_100_QuaseOrdenado))
 end_time_100_QuaseOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_100_Quase_Ordenado:", end_time_100_QuaseOrd_hs - start_time_100_QuaseOrd_hs)
+result_time_lista_10_QuaseOrd_hs = end_time_100_QuaseOrd_hs - start_time_100_QuaseOrd_hs
+print("Tempo para ordenar lista_100_Quase_Ordenado:",result_time_lista_100_QuaseOrd_hs)
 
 # Heap Sort: Lista quase ordenada: lista_1000_QuaseOrdenado
 start_time_1000_QuaseOrd_hs = time.perf_counter()
 print(hs(lista_1000_QuaseOrdenado))
 end_time_1000_QuaseOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_1000_Quase_Ordenado:", end_time_1000_QuaseOrd_hs - start_time_1000_QuaseOrd_hs)
+result_time_lista_1000_QuaseOrd_hs = end_time_1000_QuaseOrd_hs - start_time_1000_QuaseOrd_hs
+print("Tempo para ordenar lista_1000_Quase_Ordenado:", result_time_lista_1000_QuaseOrd_hs)
 
 # Heap Sort: lista inversamente ordenada ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -99,28 +108,33 @@ print("Tempo para ordenar lista_1000_Quase_Ordenado:", end_time_1000_QuaseOrd_hs
 start_time_10_InversamenteOrd_hs = time.perf_counter()
 print(hs(lista_10_InversamenteOrdenado))
 end_time_10_InversamenteOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_10_Inversamente_Ordenado:", end_time_10_InversamenteOrd_hs - start_time_10_InversamenteOrd_hs)
+result_time_lista_10_InversamenteOrd_hs = end_time_10_InversamenteOrd_hs - start_time_10_InversamenteOrd_hs
+print("Tempo para ordenar lista_10_Inversamente_Ordenado:",  result_time_lista_10_InversamenteOrd_hs)
 
 # Heap Sort: lista inversamente ordenada: lista_25_InversamenteOrdenado
 start_time_25_InversamenteOrd_hs = time.perf_counter()
 print(hs(lista_25_InversamenteOrdenado))
 end_time_25_InversamenteOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_25_Inversamente_Ordenado:", end_time_25_InversamenteOrd_hs - start_time_25_InversamenteOrd_hs)
+result_time_lista_25_InversamenteOrd_hs = end_time_25_InversamenteOrd_hs - start_time_25_InversamenteOrd_hs
+print("Tempo para ordenar lista_25_Inversamente_Ordenado:", result_time_lista_25_InversamenteOrd_hs)
 
 # Heap Sort: lista inversamente ordenada: lista_50_InversamenteOrdenado
 start_time_50_InversamenteOrd_hs = time.perf_counter()
 print(hs(lista_50_InversamenteOrdenado))
 end_time_50_InversamenteOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_50_Inversamente_Ordenado:", end_time_50_InversamenteOrd_hs - start_time_50_InversamenteOrd_hs)
+result_time_lista_50_InversamenteOrd_hs = end_time_50_InversamenteOrd_hs - start_time_50_InversamenteOrd_hs
+print("Tempo para ordenar lista_50_Inversamente_Ordenado:", result_time_lista_50_InversamenteOrd_hs)
 
 # Heap Sort: lista inversamente ordenada: lista_100_InversamenteOrdenado
 start_time_100_InversamenteOrd_hs = time.perf_counter()
 print(hs(lista_100_InversamenteOrdenado))
 end_time_100_InversamenteOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_100_Inversamente_Ordenado:", end_time_100_InversamenteOrd_hs - start_time_100_InversamenteOrd_hs)
+result_time_lista_100_InversamenteOrd_hs = end_time_100_InversamenteOrd_hs - start_time_100_InversamenteOrd_hs
+print("Tempo para ordenar lista_100_Inversamente_Ordenado:", result_time_lista_100_InversamenteOrd_hs )
 
 # Heap Sort: lista inversamente ordenada: lista_1000_InversamenteOrdenado
 start_time_1000_InversamenteOrd_hs = time.perf_counter()
 print(hs(lista_1000_InversamenteOrdenado))
 end_time_1000_InversamenteOrd_hs = time.perf_counter()
-print("Tempo para ordenar lista_1000_Inversamente_Ordenado:", end_time_1000_InversamenteOrd_hs - start_time_1000_InversamenteOrd_hs)
+result_time_lista_10_InversamenteOrd_hs = end_time_10_InversamenteOrd_hs - start_time_10_InversamenteOrd_hs
+print("Tempo para ordenar lista_1000_Inversamente_Ordenado:", result_time_lista_10_InversamenteOrd_hs )
